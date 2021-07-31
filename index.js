e098fada6bdfbd4fdbc0d7b547080f2d8c7fe72c
Ziv Kaplan <zivkaplan@gmail.com>
2021-07-31 22:42:23 +0300

    initial commit

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const mongoose = require('mongoose');
 const MongoStore = require('connect-mongo');
 const path = require('path');
 const app = express();
-const defaultRouter = require("./routes/defaultRouter")
+const defaultRouter = require('./routes/defaultRouter');
+
 // settings
 const port = process.env.PORT || 3000;
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/_____';
@@ -35,17 +36,16 @@ const appConfig = (function () {
             },
         }),
     };
+
     app.use(session(sessionConfig));
 
-    app.engine('ejs', ejsMate);
-    app.set('view engine', 'ejs');
     app.set('views', path.join(__dirname, 'views'));
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 })();
 
-const mongooseConfig = (function(){
+const mongooseConfig = (function () {
     mongoose.set('useFindAndModify', false);
 
     mongoose.connect(dbUrl, {
